28bca1b5a59d8fdc6abb526dbbe4e87b63062b74
hirakawa masaki <hirakawamasaki@hirakawanoiMac.local>
2024-02-21 00:06:19 +0900

    @GetMappingを実装しました

---
+++ src/test/java/com/kadai/kadai6/Kadai6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kadai.kadai6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kadai6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
